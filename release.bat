d6c1aab7a413267c81a26be6986b984760df3e29
William Fagan <willf@commtech-fastcom.com>
2014-03-25 11:04:11 -0500

    Add signing to release script.

--- release.bat
+++ release.bat
@@ -20,6 +20,9 @@ python setup.py build
 echo Copying Release Files...
 copy build\exe.win32-3.3\* %TOP%\ > nul
 
+:sign_files
+signtool sign /ac "DigiCert High Assurance EV Root CA.crt" /n "Commtech, Inc." /t http://timestamp.digicert.com/ %TOP%\%NAME%.exe
+
 :copy_changelog
 echo Copying ChangeLog...
 copy ChangeLog.md %TOP%\ > nul
